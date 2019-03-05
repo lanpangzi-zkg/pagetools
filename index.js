38253be13767a09ce84ff04773ac0d4b2785a725
zhangkegui <zhangkegui@fulu.com>
2019-03-05 11:46:47 +0800

    提交编译文件

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
+const static = require('koa-static');
+const path = require('path');
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const app = new Koa();
+const router = new Router();
+const staticPath = './dist';
+
+app.use(static(
+  path.join( __dirname, staticPath)
+))
+
+router.options('/generatePage', async (ctx, next) => {
+  await next();
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Request-Headers', 'Content-Type');
+  ctx.body = 'ok';
+});
+router.post('/generatePage', async (ctx, next) => {
+  await next();
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Content-Type', 'application/json');
+  ctx.set('Access-Control-Request-Headers', 'Content-Type');
+  ctx.body = {
+    code: 0,
+    message: 'success',
+  };
 });
 
-app.listen(process.env.PORT || 3000);
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(process.env.PORT || 8888);
