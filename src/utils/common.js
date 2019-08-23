2fd2d4422728fc5c4ca1baed4f808008998f0314
zhangkegui <zhangkegui@fulu.com>
2019-08-23 10:37:12 +0800

    add
    add

--- src/utils/common.js
+++ src/utils/common.js
@@ -15,6 +15,7 @@ function getProxyHeaders(reqUrl, method, acceptType, cookie, requestData) {
         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
         'X-Requested-With': 'XMLHttpRequest',
+        'X-Forwarded-For': '127.0.0.1',
         'Cache-Control': 'no-cache',
         'Pragma': 'no-cache',
     };
